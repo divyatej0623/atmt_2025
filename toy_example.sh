da34199c824977151f97c03cc7a5529a9e8a278c
divyatej0623 <divyatejreddy.kavalagari@uzh.ch>
2025-10-05 17:27:42 +0200

    Update toy_example.sh

--- toy_example.sh
+++ toy_example.sh
@@ -5,9 +5,9 @@
 #SBATCH --mem=8GB
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
-#SBATCH --output=toy_example_gpu.out
+#SBATCH --output=toy_example.out
 
-module load gpu
+# module load gpu
 module load mamba
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
